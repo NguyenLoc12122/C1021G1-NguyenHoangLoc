20d09777565df33113cac8ece7b23d8fc714402e
NguyenLoc12122 <legendragon12121991@gmail.com>
2021-12-01 16:44:31 +0700

    Bài tập và Thục hành ss1-module2

--- Module 2/src/ss1_introdution_java/bai_tap/ChuyenDoiTienTe.java
+++ Module 2/src/ss1_introdution_java/bai_tap/ChuyenDoiTienTe.java
@@ -9,8 +9,13 @@ public class ChuyenDoiTienTe {
         int result;
         Scanner scanner=new Scanner(System.in);
         System.out.println("Nhập số USD cần đổi : ");
-        USD=scanner.nextInt();
-        result=USD*23000;
-        System.out.println(result+ "VND");
+        USD = scanner.nextInt();
+        result = USD*23000;
+        System.out.println(result + "VND");
+        Scanner scanner1=new Scanner(System.in);
+        System.out.println("Nhập số VND cần đổi : ");
+        VND = scanner1.nextInt();
+        result = VND/23000;
+        System.out.println(result + "USD");
     }
 }
